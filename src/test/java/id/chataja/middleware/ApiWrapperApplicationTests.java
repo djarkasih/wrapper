ad84a0345b3a6be90493b084a3a922aff285344a
Ahmad R. Djarkasih <djarkasih@gmail.com>
2020-01-29 20:17:14 +0700

    Spring Boot REST skeleton added.

---
+++ src/test/java/id/chataja/middleware/ApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package id.chataja.middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
